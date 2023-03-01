6391416ced845bb8a2ac03de815fc49bbb6f3e3e
xhsndsy <568779761@qq.com>
2023-03-01 16:52:44 +0800

    订单验证

--- src/main/java/com/taco/tacocloud/beans/Taco.java
+++ src/main/java/com/taco/tacocloud/beans/Taco.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 @Data
 public class Taco {
-    @NonNull
+
     @Size(min = 2, message = "姓名至少有两个字")
     private String name;
-    @NonNull
+
     @Size(min = 1, message = "至少选择一个品种")
     private List<Ingredient> ingredients;
 }
--- src/main/java/com/taco/tacocloud/controller/OrderController.java
+++ src/main/java/com/taco/tacocloud/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.taco.tacocloud.controller;
 
 import com.taco.tacocloud.beans.TacoOrder;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,13 @@ public class OrderController {
         return "orderForm";
     }
 
-    @PostMapping("/current")
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+    @PostMapping("/")
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
         return "redirect:/";
